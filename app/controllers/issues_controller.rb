f1dd23e06761454acd72c1bc0f4426a6d97c1999
John Brunton <john_brunton@hotmail.co.uk>
2015-03-08 08:26:55 +0000

    sync issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,7 +4,29 @@ class IssuesController < ApplicationController
   end
   
   def sync
+    uri = URI.parse("https://jbrunton.atlassian.net/rest/api/2/search")
+    req = Net::HTTP::Get.new(uri)
+    req.basic_auth params['username'], params['password']
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
+      http.request(req)
+    end
+    
+    Issue.delete_all
+    
+    parse_response(res)
+    
     @issues = Issue.all
     render "index"
+  end
+private
+  def parse_response(response)
+    json = JSON.parse(response.body)
+    issues = json['issues']
+    issues.map do |issue_json|
+      Issue.create(
+        :key => issue_json['key'],
+        :self => issue_json['self'],
+        :summary => issue_json['fields']['summary'])
+    end
   end
 end
