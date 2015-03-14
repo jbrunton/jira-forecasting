b344a1c52e7e4fcfc74da40d672b873d5abd9d46
John Brunton <john_brunton@hotmail.co.uk>
2015-03-14 14:42:23 +0000

    #24 add scaffolding for projects

---
+++ spec/views/projects/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "projects/index", type: :view do
+  before(:each) do
+    assign(:projects, [
+      Project.create!(
+        :domain => "Domain",
+        :rapid_board_id => 1,
+        :name => "Name"
+      ),
+      Project.create!(
+        :domain => "Domain",
+        :rapid_board_id => 1,
+        :name => "Name"
+      )
+    ])
+  end
+
+  it "renders a list of projects" do
+    render
+    assert_select "tr>td", :text => "Domain".to_s, :count => 2
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+  end
+end
