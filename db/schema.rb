4f9701d272e817e27f1cede5aa7aed49c656e705
John Brunton <john_brunton@hotmail.co.uk>
2015-03-14 14:42:45 +0000

    #24 update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310092203) do
+ActiveRecord::Schema.define(version: 20150314144215) do
 
   create_table "issues", force: :cascade do |t|
     t.string   "self"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20150310092203) do
   end
 
   add_index "issues", ["epic_id"], name: "index_issues_on_epic_id"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "domain"
+    t.integer  "rapid_board_id"
+    t.string   "name"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "wip_histories", force: :cascade do |t|
     t.date     "date"
